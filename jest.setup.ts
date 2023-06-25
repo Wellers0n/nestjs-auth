065d7900167809cfbab0116b31afe4249b4b34c6
Wellerson <wllrsn.c@gmail.com>
2023-06-25 18:35:08 -0300

    refactor: remove comments

--- jest.setup.ts
+++ jest.setup.ts
@@ -11,9 +11,6 @@ beforeAll(async () => {
   );
 });
 
-// if you need to add a handler after calling setupServer for some specific test
-// this will remove that handler for the rest of them
-// (which is important for test isolation):
 afterAll(async () => {
   const client = new Client({
     connectionString: process.env.DATABASE_TEST_URL,
