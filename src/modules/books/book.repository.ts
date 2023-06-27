04f3f82dc54839d0ca6c73b20a8292d53630d998
Wellerson <wllrsn.c@gmail.com>
2023-06-27 11:48:14 -0300

    feat: create book repository

---
+++ src/modules/books/book.repository.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/database/prismaServices';
+import { BookEntity } from './entities/book.entity';
+
+@Injectable()
+export class BookRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(
+    title: string,
+    description: string,
+    user_id: number,
+  ): Promise<BookEntity> {
+    return this.prisma.book.create({
+      data: {
+        title,
+        description,
+        user_id,
+      },
+    });
+  }
+
+  async find(title: string): Promise<BookEntity[]> {
+    return this.prisma.book.findMany({
+      where: {
+        title: {
+          contains: title,
+        },
+      },
+    });
+  }
+  async findOne(id: number): Promise<BookEntity> {
+    return this.prisma.book.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.prisma.book.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(
+    id: number,
+    title: string,
+    description: string,
+  ): Promise<BookEntity> {
+    return this.prisma.book.update({
+      data: {
+        title,
+        description,
+      },
+      where: {
+        id,
+      },
+    });
+  }
+}
